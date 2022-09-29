d8d34898fa19f2c5948b85c726c03976de6d2fc5
朱作林 <zhuzl@hello-tech.com>
2022-09-29 16:22:49 +0800

    2222

--- build/push.js
+++ build/push.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2022-09-29 11:28:42
  * @LastEditors: Leo
- * @LastEditTime: 2022-09-29 16:17:09
+ * @LastEditTime: 2022-09-29 16:19:16
  * @FilePath: \webpack-shopify\build\push.js
  * @description: 修改模板上传至模板git
  */
@@ -9,13 +9,14 @@ const shell = require("shelljs");
 const exec = shell.exec;
 const read = require('read-yaml');
 const config = read.sync('config.yml');
-let name = process.argv[2] || 'ca';
+let name = process.argv[2] || 'ca'; // 第一个参数传店铺
+let branch = process.argv[3] || 'master'; // 第二个参数传分支名称
 
 if(!config[name]) {
     shell.echo(`Error: 店铺参数${name}错误`);
     shell.exit(1);
 }
-console.log(`${name}店铺模板同步至shopify模板中...`)
+console.log(`${name}店铺模板同步至shopify模板git中...`)
 
 // exec(`theme deploy --dir=theme --env=${name}`)
 
