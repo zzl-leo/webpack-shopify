0a97fd5949d5c2222e21ef35a00b9ff36a606186
朱作林 <zhuzl@hello-tech.com>
2022-09-30 14:49:06 +0800

    1213

--- build/deploy.js
+++ build/deploy.js
@@ -2,8 +2,8 @@
 /*
  * @Date: 2022-09-29 10:23:32
  * @LastEditors: Leo
- * @LastEditTime: 2022-09-29 11:31:00
- * @FilePath: \shopify-starter-theme-master\build\deploy.js
+ * @LastEditTime: 2022-09-30 14:47:50
+ * @FilePath: \test2\build\deploy.js
  * @description: 构建并发布到指定模板命令（theme清空-theme get-构建-theme 同步到店铺模板）
  */
 const shell = require("shelljs");
@@ -18,4 +18,5 @@ if(!config[name]) {
 }
 console.log(`${name}店铺模板开始构建并publish...`)
 
-exec(`npm run get ${name} && cross-env process.env.SHOPIFY_STORE=${name} webpack --config webpack.config.${name}.js --mode=production && npm run push ${name}`)
+// exec(`npm run get ${name} && cross-env process.env.SHOPIFY_STORE=${name} webpack --config webpack.config.${name}.js --mode=production && npm run push ${name}`)
+exec(`cross-env process.env.SHOPIFY_STORE=${name} webpack --config webpack.config.${name}.js --mode=production && npm run push ${name}`)
