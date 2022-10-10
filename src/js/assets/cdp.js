2b9b46da12972151f77d9efd9d40454e7d907ecf
朱作林 <zhuzl@hello-tech.com>
2022-10-10 16:15:11 +0800

    2313

--- src/js/assets/cdp.js
+++ src/js/assets/cdp.js
@@ -1,7 +1,18 @@
 /*
  * @Date: 2022-10-10 11:00:37
  * @LastEditors: Leo
- * @LastEditTime: 2022-10-10 11:33:01
+ * @LastEditTime: 2022-10-10 16:14:11
  * @FilePath: \test2\src\js\assets\cdp.js
  */
-console.log(Shopify)
+import jackerySensors from '../utils/ja.js'
+
+window.jackerySensors.init({
+    server_url: 'https://cdpdata.myjackery.com/sa',
+    send_type: 'beacon',
+    heatmap: {
+        clickmap: 'default',
+        scroll_notice_map: 'default'
+    }
+});
+
+window.jackerySensors.quick('autoTrack');
--- src/js/utils/index.js
+++ src/js/utils/index.js
@@ -1,9 +1,20 @@
 /*
  * @Date: 2022-09-22 18:17:55
  * @LastEditors: Leo
- * @LastEditTime: 2022-09-22 18:22:06
- * @FilePath: \shopify-starter-theme-master\src\js\utils\index.js
+ * @LastEditTime: 2022-10-10 16:08:17
+ * @FilePath: \test2\src\js\utils\index.js
  */
+
+// query 格式化
+export const urlQueryToObject = (url = window.location.href) => {
+    const querys = url.split('?')[1] || []
+    if (querys.length > 0) {
+        return JSON.parse('{"' + decodeURI(querys).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}')
+    }
+    return {}
+}
+
+
 export const withInstall = (component, alias) => {
     const comp = component;
     comp.install = (app) => {
