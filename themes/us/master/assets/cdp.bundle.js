8e32fddc660fd721132c40df0485f86ff5a6e936
朱作林 <zhuzl@hello-tech.com>
2022-10-10 16:01:06 +0800

    12

--- themes/us/master/assets/cdp.bundle.js
+++ themes/us/master/assets/cdp.bundle.js
@@ -1,7 +1,9 @@
 /*
  * @Date: 2022-10-10 15:52:55
  * @LastEditors: Leo
- * @LastEditTime: 2022-10-10 15:52:56
+ * @LastEditTime: 2022-10-10 16:00:55
  * @FilePath: \test2\themes\us\master\assets\cdp.bundle.js
  */
+console.log('88888888888');
 console.log(Shopify);
+console.log('88888888888');
