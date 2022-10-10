5e1520bca61fc7b181818fa5669c09fe78100809
朱作林 <zhuzl@hello-tech.com>
2022-10-10 15:53:10 +0800

    1

---
+++ themes/us/master/assets/cdp.bundle.js
@@ -0,0 +1,7 @@
+/*
+ * @Date: 2022-10-10 15:52:55
+ * @LastEditors: Leo
+ * @LastEditTime: 2022-10-10 15:52:56
+ * @FilePath: \test2\themes\us\master\assets\cdp.bundle.js
+ */
+console.log(Shopify);
