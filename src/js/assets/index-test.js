09e9f8153acb48450cd19cf7641af43f6e46ef1e
朱作林 <zhuzl@hello-tech.com>
2022-11-17 14:58:34 +0800

    123432432

---
+++ src/js/assets/index-test.js
@@ -0,0 +1,7 @@
+/*
+ * @Date: 2022-11-17 14:58:16
+ * @LastEditors: Leo
+ * @LastEditTime: 2022-11-17 14:58:16
+ * @FilePath: \test2\src\js\assets\index-test.js
+ */
+console.log("hello------------")
